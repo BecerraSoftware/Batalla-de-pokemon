2a0afd87033fcea514dab2c4d7ef6c10a0ead3dd
VictorBecerra <152622280+BecerraSoftware@users.noreply.github.com>
2024-12-07 12:15:06 -0600

    Agregar archivos de proyecto.

---
+++ ORDINARIO RAUL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORDINARIO_RAUL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
